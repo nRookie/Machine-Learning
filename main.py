061ea6957975fe11b1b0afc1d6700021be366b5d
nrookie <228611676@qq.com>
2022-05-07 19:28:35 +0800

    fix(neural): add epoch 10 and learning rate to 0.25

--- main.py
+++ main.py
@@ -214,7 +214,7 @@ def whole_set_big():
     hidden_nodes = 100
     output_nodes = 10
 
-    learning_rate = 0.3
+    learning_rate = 0.25
 
     # create instance of neural network
     n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
@@ -272,4 +272,69 @@ def whole_set_big():
     print ("performance percentage = ", (scorecard_array.sum() / scorecard_array.size)*100,"%")
 
 
-whole_set_big()
+
+
+def epoch_whole_set():
+    input_nodes = 784
+    hidden_nodes = 100
+    output_nodes = 10
+
+    learning_rate = 0.25
+
+    # create instance of neural network
+    n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+
+    # load the mnist training data CSV file into a list
+
+    training_data_file = open("mnist_train.csv", 'r')
+    training_data_list = training_data_file.readlines()
+    training_data_file.close()
+    epochs = 10
+    # train the neural network
+    for e in range(epochs):
+        for record in training_data_list:
+            # split the record by the ',' commas
+            all_values = record.split(',')
+            # scale and shift the inputs
+            inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+            targets = numpy.zeros(output_nodes) + 0.01
+            # all_values[0] is the target label for this record 
+            targets[int(all_values[0])] = 0.99
+            n.train(inputs, targets)
+        pass
+    pass
+    scorecard =[]
+
+    test_data_file = open("mnist_test.csv", 'r')
+    test_data_list = test_data_file.readlines()
+    test_data_file.close()
+    print(len(test_data_list))
+
+    for record in test_data_list:
+        all_values = record.split(',')
+
+        correct_label = int(all_values[0])
+        # print(correct_label, "correct label")
+
+        # scale and shift the inputs
+        inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+        # query the network
+        outputs = n.query(inputs)
+        # the index of the highest value corresponds to the label
+        label = numpy.argmax(outputs)
+        # print(label, "network's answer")
+
+        # append correct or incorrect to list 
+        if (label == correct_label):
+            # network's answer matches correct answer, add 1 to scorecard
+            scorecard.append(1)
+        else:
+            # network's answer doesn't match correct answer, add 0 to scorecard
+            scorecard.append(0)
+
+    print(scorecard)
+    scorecard_array = numpy.asarray(scorecard)
+    print ("performance = ", scorecard_array.sum() / scorecard_array.size)
+    print ("performance percentage = ", (scorecard_array.sum() / scorecard_array.size)*100,"%")
+
+epoch_whole_set()
